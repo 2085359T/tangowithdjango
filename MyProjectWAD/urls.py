ad9c5d446f7cd0625398deaa83495722f41e27e0
unknown <2085359T@BO706-42.ad.dcs.gla.ac.uk>
2015-01-23 19:04:46 +0000

    Chapter 7 Completed

--- MyProjectWAD/urls.py
+++ MyProjectWAD/urls.py
@@ -3,12 +3,8 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^rango/', include('rango.urls')),
+    url(r'^rango/', include('rango.urls')),
 )
 
 if settings.DEBUG:
